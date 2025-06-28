7d25f62e9c3c02b9b378e8806e30fc2f588f806a
Arnstein Snyen Marjala <arnstein@giof.se>
2025-06-28 15:16:20 +0200

    Fix Vite build configuration error

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html')
-      }
-    }
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
